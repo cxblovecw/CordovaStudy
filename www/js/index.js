1a60aa02d703951e4838e13bec1652a4a34fd481
cxblovecw <873587091@qq.com>
2020-04-30 17:04:07 +0800

    network

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,21 @@
 document.addEventListener('deviceReady',function(){
-    window.addEventListener('batterystatus',function(status){
-        console.log(status)
-        alert("BATTERY STATUS:  Level: " + status.level + " isPlugged: " + status.isPlugged)
-    },false)
-    window.addEventListener('batterylow',function(status){
-        alert("BATTERY STATUS:  Level: " + status.level + " isPlugged: " + status.isPlugged)
-    },false)
-    
-    window.addEventListener('batterycritical',function(status){
-        alert("BATTERY STATUS:  Level: " + status.level + " isPlugged: " + status.isPlugged)
-    },false)
+   document.getElementById('network').onclick=function(){
+       let status={}
+       status[Connection.NONE]=["离线状态"];
+       status[Connection.WIFI]=["WIFT状态"];
+       status[Connection.CELL]=["1G状态？？"];
+       status[Connection.CELL_2G]=["2G状态"];
+       status[Connection.CELL_3G]=["3G状态"];
+       status[Connection.CELL_4G]=["4G状态"];
+       status[Connection.ETHERNET]=["连接的是以太网"];
+       status[Connection.UNKNOWN]=["未知状态"];
+       alert(navigator.connection.type);         // 当前状态
+       alert(`您当前的状态为:${status[navigator.connection.type]}`);   
+   }
+   document.addEventListener('online',function(){
+       alert("在线状态");
+   })
+   document.addEventListener('offline',function(){
+       alert('离线状态');
+   })
 })
