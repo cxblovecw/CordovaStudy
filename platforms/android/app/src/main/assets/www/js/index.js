9203b9426bc584b62a1b644a15b641d71803d354
cxblovecw <873587091@qq.com>
2020-04-30 10:22:36 +0800

    battery

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,22 +1,13 @@
-document.getElementById('set').onclick=function(){
-    localStorage.setItem('name','xxx');
-    localStorage.setItem('age',18);
-}
-document.getElementById('get').onclick=function(){
-    alert(localStorage.getItem('name')+localStorage.getItem('age'));
-}
-document.getElementById('remove').onclick=function(){
-    // 指定单个删除
-    localStorage.removeItem('name');
-}
-document.getElementById('getByKey').onclick=function(){
-    // key是根据字母顺序的 字母越小排越前
-    alert(localStorage.key(0))
-}
-document.getElementById('length').onclick=function(){
-    alert(localStorage.length);
-}
-document.getElementById('clear').onclick=function(){
-    // 全部删除
-    localStorage.clear();
-}
+document.addEventListener('deviceReady',function(){
+    window.addEventListener('batterystatus',function(status){
+        console.log(status)
+        alert("BATTERY STATUS:  Level: " + status.level + " isPlugged: " + status.isPlugged)
+    },false)
+    window.addEventListener('batterylow',function(status){
+        alert("BATTERY STATUS:  Level: " + status.level + " isPlugged: " + status.isPlugged)
+    },false)
+    
+    window.addEventListener('batterycritical',function(status){
+        alert("BATTERY STATUS:  Level: " + status.level + " isPlugged: " + status.isPlugged)
+    },false)
+})
