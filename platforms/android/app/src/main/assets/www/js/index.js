75dc8cc00cc28e62b3220a77c8c9c3e8ed60b586
cxblovecw <873587091@qq.com>
2020-05-01 23:59:35 +0800

    vibration

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,22 +1,18 @@
-document.getElementById('set').onclick=function(){
-    localStorage.setItem('name','xxx');
-    localStorage.setItem('age',18);
-}
-document.getElementById('get').onclick=function(){
-    alert(localStorage.getItem('name')+localStorage.getItem('age'));
-}
-document.getElementById('remove').onclick=function(){
-    // 指定单个删除
-    localStorage.removeItem('name');
-}
-document.getElementById('getByKey').onclick=function(){
-    // key是根据字母顺序的 字母越小排越前
-    alert(localStorage.key(0))
-}
-document.getElementById('length').onclick=function(){
-    alert(localStorage.length);
-}
-document.getElementById('clear').onclick=function(){
-    // 全部删除
-    localStorage.clear();
-}
+document.addEventListener('deviceReady',function(){
+  document.getElementById('vibration').onclick=function(){
+    // 振动时间
+    // ios忽略指定的时间 使用系统预设的时间
+    // window最长5秒 最短1ms
+    navigator.vibrate(2000);
+  }
+  document.getElementById('sustained-vibration').onclick=function(){
+    // 模式振动
+    // 振动一秒 停一秒 振动两秒 停两秒 振动三秒 停三秒
+    // 
+    navigator.vibrate([1000,1000,2000,2000,3000])
+  }
+  document.getElementById('stopVibration').onclick=function(){
+    // 将时间设置为 0 即可消除振动
+    navigator.vibrate(0);
+  }
+})
